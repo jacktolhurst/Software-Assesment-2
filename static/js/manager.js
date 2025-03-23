e77d05629c90f9b763cb4d9447aabd76ed38d871
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2025-03-23 19:36:12 +1100

    Better password detection

--- static/js/manager.js
+++ static/js/manager.js
@@ -1,15 +1,36 @@
-window.setInterval(CheckPassword, 20);
+let text;
 
-function CheckPassword() {
-  var password = document.getElementById("password").value;
-  if (IsStrongPassword(password)) {
-    var btn = document.getElementById("btn");
-    btn.style.color = "black";
-    btn.disabled = false;
+function CheckInputs() {
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+  var DOB = document.getElementById("DOB");
+  var btn = document.getElementById("btn");
+
+  if (username.value != "") {
+    if (IsStrongPassword(password.value)) {
+      if (IsValidDate(DOB.value)) {
+        text.textContent = "";
+        btn.disabled = false;
+        btn.style.cursor = "pointer";
+        btn.style.color = "black";
+      } else {
+        text.textContent = "Date must be in a DD/MM/YYYY format";
+        btn.disabled = true;
+        btn.style.cursor = "default";
+        btn.style.color = "grey";
+      }
+    } else {
+      text.textContent =
+        "A password must be at least 8 letters, have one capital letter and one special character";
+      btn.disabled = true;
+      btn.style.cursor = "default";
+      btn.style.color = "grey";
+    }
   } else {
-    var btn = document.getElementById("btn");
-    btn.style.color = "grey";
+    text.textContent = "Must be a vaild username";
     btn.disabled = true;
+    btn.style.cursor = "default";
+    btn.style.color = "grey";
   }
 }
 
@@ -31,3 +52,30 @@ function IsStrongPassword(password) {
   }
   return true;
 }
+
+function IsValidDate(dateString) {
+  const regex = /^(0[1-9]|1[0-2])\/([0-2][0-9]|3[01])\/\d{4}$/;
+  if (!regex.test(dateString)) {
+    return false;
+  }
+
+  const [month, day, year] = dateString.split("/").map(Number);
+  const date = new Date(year, month - 1, day);
+
+  return (
+    date.getMonth() === month - 1 &&
+    date.getDate() === day &&
+    date.getFullYear() === year
+  );
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const inputFields = document.querySelectorAll(".input__field");
+  text = document.getElementById("description_text");
+
+  inputFields.forEach((input) => {
+    input.addEventListener("input", CheckInputs);
+  });
+
+  CheckInputs();
+});
